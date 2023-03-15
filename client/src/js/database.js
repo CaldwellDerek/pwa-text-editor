23c317d34087331cae05aa3ec42f0e50b806f62c
Derek Caldwell <djohncaldwell@gmail.com>
2023-03-14 19:48:03 -0700

    asset caching corrected

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,11 +3,11 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate_table')) {
+      if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate_table', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
@@ -15,8 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jate = await openDB("jate", 1);
-  const transaction = jate.transaction('jate_table', 'readwrite');
-  const store = transaction.objectStore('jate_table');
+  const transaction = jate.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
   const request = store.add({content});
   const result = await request;
 };
@@ -24,8 +24,8 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jate = await openDB("jate", 1);
-  const transaction = jate.transaction('jate_table', 'readonly');
-  const store = transaction.objectStore('jate_table');
+  const transaction = jate.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
   const request = store.getAll();
   const result = await request;
 };
