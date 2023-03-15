9d332fdde11288c4a5740b8da577501be6531cd7
Derek Caldwell <djohncaldwell@gmail.com>
2023-03-14 17:24:00 -0700

    putDb defined

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,17 +3,23 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jate_table')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate_table', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jate = await openDB("jate", 1);
+  const transaction = jate.transaction('jate_table', 'readwrite');
+  const store = transaction.objectStore('jate_table');
+  const request = store.add({content});
+  const result = await request;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
